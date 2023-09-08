0d69af5b1ed5b7ebb3a9f4fc804de2e31cc7b413
haris_delalic <delalicharis@gmail.com>
2023-09-08 17:17:17 +0200

    initial commit

---
+++ app/src/main/java/com/delalic/todolistapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.delalic.todolistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.delalic.todolistapp.ui.theme.ToDoListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListAppTheme {
+
+                Greeting("dela")
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = name)
+}
---
+++ app/src/main/java/com/delalic/todolistapp/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.delalic.todolistapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+
+)
+
+private val LightColorPalette = lightColors(
+
+)
+
+@Composable
+fun ToDoListAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        content = content
+    )
+}

b54723b798e984a755a5d807e07b764f8619c96b
haris_delalic <delalicharis@gmail.com>
2023-09-10 17:26:14 +0200

    design searc app bar

--- app/src/main/java/com/delalic/todolistapp/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/delalic/todolistapp/ui/screens/list/ListAppBar.kt
@@ -1,36 +1,54 @@
 package com.delalic.todolistapp.ui.screens.list
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.ContentAlpha.medium
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.delalic.todolistapp.R
 import com.delalic.todolistapp.components.PriorityItem
 import com.delalic.todolistapp.data.enums.Priority
+import com.delalic.todolistapp.ui.theme.TOP_APP_BAR_HEIGHT
 import com.delalic.todolistapp.ui.theme.topAppBarBackgroundColor
 import com.delalic.todolistapp.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar(
-        onSearchClicked = {},
-        onSortClicked = {},
-        onDeleteAllClicked = {}
-    )
+//    DefaultListAppBar(
+//        onSearchClicked = {},
+//        onSortClicked = {},
+//        onDeleteAllClicked = {}
+//    )
+    SearchAppBar(text = "search", onTextChange = {}, onCloseClicked = {}, onSearchClicked = {})
 }
 
 @Composable
@@ -143,8 +161,90 @@ fun DeleteAllAction(
     }
 }
 
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBackgroundColor
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            onValueChange = onTextChange,
+            placeholder = {
+                Text(
+                    text = "Search", //! transfer to strings.xml
+                    color = Color.White,
+                    modifier = Modifier
+                        .alpha(medium)
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .alpha(ContentAlpha.disabled)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon", //! transfer to strings.xml
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        onCloseClicked()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon", //! transfer to strings.xml
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colors.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent
+            )
+        )
+    }
+}
+
 @Preview
 @Composable
 fun PreviewDefaultTopAppBar() {
     DefaultListAppBar(onSearchClicked = {}, onSortClicked = {}, onDeleteAllClicked = {})
+}
+
+@Preview
+@Composable
+fun PreviewSearchAppBar() {
+    SearchAppBar(text = "search", onTextChange = {}, onCloseClicked = {}, onSearchClicked = {})
 }
